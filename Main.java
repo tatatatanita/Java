05efeb7d112218b77e002baf5153c122699e1fe4
tatatatanita <y_shibata_nagoya@yahoo.co.jp>
2020-02-14 17:22:56 +0900

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+
+Int number;
+number = 3;
+ 
+ System.out.println( );
+ System.out.println("こんにちは"+"世界" );
+
+Int number = 3;
+String text = “Hello World"
+  }
+}
